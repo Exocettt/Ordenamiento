921e4f86c5dd2b5f0ccff86c0a2a7f2eb18b0829
Jesus Noriega <jesusadan049@gmail.com>
2023-06-26 22:25:31 -0700

    proyecto listas simples

--- Metodo de Ordenamiento/Metodo de Ordenamiento/Nodo.cs
+++ Metodo de Ordenamiento/Metodo de Ordenamiento/Nodo.cs
@@ -8,5 +8,13 @@ namespace Metodo_de_Ordenamiento
 {
     internal class Nodo
     {
+        public string Valor { get; set; }
+        public Nodo Enlace { get; set; }
+
+        public Nodo(string valor = "", Nodo enlace = null)
+        {
+            Valor = valor;
+            Enlace = enlace;
+        }
     }
 }
